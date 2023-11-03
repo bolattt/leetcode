c153422f8cd80a54d53afe0976d6f7bfb811e353
Bo Latt <83052118+bolattt@users.noreply.github.com>
2023-11-03 11:56:01 -0400

    Time: 58 ms (44.75%), Space: 43.1 MB (16.85%) - LeetHub

--- 0167-two-sum-ii-input-array-is-sorted/0167-two-sum-ii-input-array-is-sorted.js
+++ 0167-two-sum-ii-input-array-is-sorted/0167-two-sum-ii-input-array-is-sorted.js
@@ -4,20 +4,36 @@
  * @return {number[]}
  */
 var twoSum = function(numbers, target) {
-  let left = 0; 
-  let right = numbers.length - 1;
 
-    while( left < right) { 
-        let sum = numbers[left] + numbers[right]
-        if(sum == target) { 
-            return [left + 1, right +1]
-        } else if(sum > target) { 
-            right--
-        } else { 
-            left++
-        }
+
+    for(let i = 0; i < numbers.length; i++) { 
+          let left = i; 
+          let right = numbers.length - 1; 
+          let complement =  target - numbers[i]
+
+        while(left <= right) { 
+            const mid = Math.ceil((left + right) / 2) 
+            const midVal = numbers[mid]
+            
+            if(midVal == complement) return [i +1, mid +1]
+            else if(midVal > complement) right = mid - 1
+            else left = mid + 1
+        } 
+        
     }
     
 };
 
 
+// [2,3,4]  target = 6 
+// left 0 , right 2 , mid 1 ,com 4 , midVal 3 
+// left 2, right 2, mid 2, com 4, midVal 4 
+
+// [1,2,3,4,4,9,56,90] target = 8 
+// i 0, left 0, right 7, mid 3, midVal 4, complement 7 
+// i 0, left 4, right 7, mid 5, midVal 9, complement 7 
+// i 0, left 4, right 4,  mid 4, midVal 4, complement 7 
+// i 0, left 5, right 4
+
+// i 3, left 3, right 7, mid 5, midVal 9, complement 4
+// i 3, left 3, right 4, mid 3, midVal 4, complement 4
