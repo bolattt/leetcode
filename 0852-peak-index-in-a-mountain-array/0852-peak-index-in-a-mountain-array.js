d2713dd23e09ab9d22f138c322a9d9855622a3ab
Bo Latt <83052118+bolattt@users.noreply.github.com>
2023-11-03 20:06:33 -0400

    Time: 53 ms (94.80%), Space: 51.1 MB (68.78%) - LeetHub

--- 0852-peak-index-in-a-mountain-array/0852-peak-index-in-a-mountain-array.js
+++ 0852-peak-index-in-a-mountain-array/0852-peak-index-in-a-mountain-array.js
@@ -3,12 +3,17 @@
  * @return {number}
  */
 var peakIndexInMountainArray = function(arr) {
-    let left = 0; 
-    let right = 1;
+   let left = 0 ;
+    let right = arr.length - 1; 
     
-    while(arr[left] < arr[right]) { 
-        left++
-        right++
+    while(left <= right) { 
+        const mid = Math.floor((left + right) / 2) 
+        const peak = arr[mid] 
+        if(peak > arr[mid -1] && peak > arr[mid + 1] ) return mid 
+        else if(peak < arr[mid - 1]) right = mid - 1
+        else left = mid + 1 
     }
-    return --right
 };
+    
+    // [0,10,8,5,2]
+    // [0,2,5,8,10,0]
