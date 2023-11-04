f45d8963b124fd8b6c17bd33598c829134fbe524
Bo Latt <83052118+bolattt@users.noreply.github.com>
2023-11-04 10:36:16 -0400

    Time: 59 ms (89.98%), Space: 44.6 MB (19.82%) - LeetHub

--- 1365-how-many-numbers-are-smaller-than-the-current-number/1365-how-many-numbers-are-smaller-than-the-current-number.js
+++ 1365-how-many-numbers-are-smaller-than-the-current-number/1365-how-many-numbers-are-smaller-than-the-current-number.js
@@ -3,18 +3,25 @@
  * @return {number[]}
  */
 var smallerNumbersThanCurrent = function(nums) {
+    // after sorting, index will be the num of smaller nums, ignore same items
     const sorted = [...nums].sort((a,b) => a-b)
     // [8,1,2,2,3]
     // [1,2,3,3,8]
+    
+    // key is num, value is number of items smaller than num
     const obj = { }
+    const ans = [] 
+    
+    // one pass to count num of items smaller than cur num 
     for(let i = 0; i < nums.length; i++) { 
-        const val = sorted[i]
-        if(obj[val] == undefined )  { 
-            obj[val] = i 
+        const num = sorted[i]
+        if(obj[num] == undefined )  { 
+            obj[num] = i 
         }
     }
     
-    const ans = []
+    // another pass to push num of smaller items to array
+    // need 2 passes coz input array is not sorted
     for(let i = 0; i < nums.length; i++) { 
         ans.push(obj[nums[i]])
     }
