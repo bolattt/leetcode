2640495dbd14894ca6df88eb277c5ceabdc60ec0
Bo Latt <83052118+bolattt@users.noreply.github.com>
2023-11-04 11:11:32 -0400

    Time: 81 ms (12.71%), Space: 45.4 MB (10.08%) - LeetHub

--- 1365-how-many-numbers-are-smaller-than-the-current-number/1365-how-many-numbers-are-smaller-than-the-current-number.js
+++ 1365-how-many-numbers-are-smaller-than-the-current-number/1365-how-many-numbers-are-smaller-than-the-current-number.js
@@ -9,21 +9,22 @@ var smallerNumbersThanCurrent = function(nums) {
     // [1,2,3,3,8]
     
     // key is num, value is number of items smaller than num
-    const obj = { }
+    const countMap = new Map();
     const ans = [] 
     
     // one pass to count num of items smaller than cur num 
     for(let i = 0; i < nums.length; i++) { 
         const num = sorted[i]
-        if(obj[num] == undefined )  { 
-            obj[num] = i 
-        }
+       if(!countMap.has(num)) { 
+            countMap.set(num,i)
+       }
     }
     
     // another pass to push num of smaller items to array
     // need 2 passes coz input array is not sorted
+    console.log(countMap)
     for(let i = 0; i < nums.length; i++) { 
-        ans.push(obj[nums[i]])
+        ans.push(countMap.get(nums[i]))
     }
  
     return ans
