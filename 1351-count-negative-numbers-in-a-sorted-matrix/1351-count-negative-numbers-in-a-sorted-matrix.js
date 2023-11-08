84b67c2f4c135e6d68f09996e8a9a9fc39e4a91d
Bo Latt <83052118+bolattt@users.noreply.github.com>
2023-11-07 19:32:33 -0500

    Time: 57 ms (42.80%), Space: 43.7 MB (80.79%) - LeetHub

--- 1351-count-negative-numbers-in-a-sorted-matrix/1351-count-negative-numbers-in-a-sorted-matrix.js
+++ 1351-count-negative-numbers-in-a-sorted-matrix/1351-count-negative-numbers-in-a-sorted-matrix.js
@@ -7,16 +7,22 @@ var countNegatives = function(grid) {
     for(let row of grid) { 
         let left = 0; 
         let right = row.length - 1; 
+     
         
         if(row[left] >= 0) { 
-            // look for first negative 
-            // after the while loop,left will be pointing at first negative num
-            while(left <= right) { 
-                const mid = Math.floor((left + right) / 2) 
-                if(row[mid] < 0) right = mid - 1
-                else if(row[mid] >= 0) left = mid + 1
+            if(row[right] >= 0)  { 
+                left = row.length
+            } else { 
+                   // look for first negative 
+                // after the while loop,left will be pointing at first negative num
+                while(left <= right) { 
+                    const mid = Math.floor((left + right) / 2) 
+                    if(row[mid] < 0) right = mid - 1
+                    else if(row[mid] >= 0) left = mid + 1
+                }
             }
-        }
+         
+        } 
    
         const numOfNegativesInRow = row.length - left 
         count += numOfNegativesInRow
