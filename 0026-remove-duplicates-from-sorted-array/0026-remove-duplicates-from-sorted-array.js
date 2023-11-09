695bef617ba5462cf0bb1ba1e4abec1fb72ebc87
Bo Latt <83052118+bolattt@users.noreply.github.com>
2023-11-09 09:52:36 -0500

    Time: 69 ms (37.40%), Space: 44.1 MB (87.56%) - LeetHub

---
+++ 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.js
@@ -0,0 +1,15 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+
+ */
+var removeDuplicates = function(nums) {
+    let left = 0; 
+    for(let right = 1; right < nums.length; right++) { 
+        if(nums[left]  != nums[right]) { 
+            nums[++left] = nums[right] 
+        }
+    }
+    
+    return left + 1 
+}
