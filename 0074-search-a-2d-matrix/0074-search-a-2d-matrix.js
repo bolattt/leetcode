82dedd979d9eb23d9524ddda71e4133462996d65
Bo Latt <83052118+bolattt@users.noreply.github.com>
2023-11-10 12:29:11 -0500

    Time: 52 ms (55.54%), Space: 44 MB (6.01%) - LeetHub

--- 0074-search-a-2d-matrix/0074-search-a-2d-matrix.js
+++ 0074-search-a-2d-matrix/0074-search-a-2d-matrix.js
@@ -4,29 +4,32 @@
  * @return {boolean}
  */
 var searchMatrix = function(matrix, target) {
-    // loop through rows 
-    // check if target is within row's range 
-        // if yes, do binary search, if found return , if not return false 
-    // if not within range, continue to the next row 
+    let top = 0
+    let bottom = matrix.length - 1
+    let midRow
     
-    for(let row of matrix) { 
-        let left=  0 
-        let right = row.length -1 
+    while(top <= bottom) { 
+        let mid =   Math.floor((top + bottom) / 2 ) 
+         midRow = matrix[mid]
         
-        if(target == row[left] || target == row[right] ) return true
-        console.log({target,left,right,})
-        console.log(row[left],row[right])
-        if(target > row[left] && target < row[right]) {  
-        
-            while(left <= right) { 
-                let mid = Math.floor((left + right) / 2 ) 
-                console.log({mid})
-                if(row[mid] == target) return true 
-                else if(row[mid] <  target) left = mid + 1 
-                else right = mid - 1 
-            }
-            return false 
-        }
+        if(target < midRow[0]) bottom = mid -  1
+        else if (target > midRow[midRow.length -1]) top = mid + 1 
+        else break; 
+    }
+    console.log({top,bottom})
+    // after while loop, target will be within range of midRow
+    // if target is not within range of all rows, top will be greater than bottom 
+    if(top > bottom) return false 
+    
+    let left = 0; 
+    let right = matrix[0].length - 1 
+
+    while(left <= right) { 
+        let mid = Math.floor((left + right) / 2)
+        console.log({left,right,mid})
+        if(target == midRow[mid]) return true 
+        else if(target < midRow[mid]) right  =  mid - 1 
+        else left = mid + 1 
     }
     return false 
 };
