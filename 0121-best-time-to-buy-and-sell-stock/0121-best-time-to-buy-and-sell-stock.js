3685d5891ad9d32f235c0dcdb743462c28d38d18
Bo Latt <83052118+bolattt@users.noreply.github.com>
2023-11-11 09:57:25 -0500

    Time: 77 ms (32.45%), Space: 51.6 MB (44.76%) - LeetHub

---
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.js
@@ -0,0 +1,24 @@
+/**
+ * @param {number[]} prices
+ * @return {number}
+ */
+var maxProfit = function(prices) {
+    let left = 0; 
+    let right = 1; 
+    let maxProfit = 0; 
+    
+    while(right < prices.length) { 
+        if( prices[right] < prices[left]) { 
+             left = right 
+        }
+        
+        let profit = prices[right] -  prices[left] 
+        if(profit > maxProfit) { 
+            maxProfit = profit
+        }
+        right++;
+    }
+    
+    return maxProfit 
+};
+
