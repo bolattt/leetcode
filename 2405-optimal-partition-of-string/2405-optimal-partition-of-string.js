6743697ea2e9088c49c302025613bb8d2a181b36
Bo Latt <83052118+bolattt@users.noreply.github.com>
2023-11-12 14:56:05 -0500

    Time: 488 ms (9.80%), Space: 116.4 MB (7.44%) - LeetHub

--- 2405-optimal-partition-of-string/2405-optimal-partition-of-string.js
+++ 2405-optimal-partition-of-string/2405-optimal-partition-of-string.js
@@ -2,24 +2,9 @@
  * @param {string} s
  * @return {number}
    no repeating ch in substring 
-   
-   init numOfSubString  = 0 
-   init seen (obj) // { 'a' : true, 'ba', true }
-   
-   loop through the string 
-     create a variable to hold subString 
-   
-     if ch is already in the subString 
-         track the subString that's already seen 
-         increment the numOfSubString 
-         
-      else if not in the subString 
-        add it to the sub string 
-        
-    return numOfSubString 
+    
  */
 var partitionString = function(s) {
-    let subStr = ''
     let seen = {}
     let count = 1
     
