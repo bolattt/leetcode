ae5496d1ea5a2c8f9efc3543cfaaa9c6ceb7a6c6
Bo Latt <83052118+bolattt@users.noreply.github.com>
2023-11-13 09:29:36 -0500

    Time: 70 ms (18.18%), Space: 43.6 MB (65.22%) - LeetHub

--- 0258-add-digits/0258-add-digits.js
+++ 0258-add-digits/0258-add-digits.js
@@ -1,11 +1,17 @@
 /**
  * @param {number} num
  * @return {number}
-
-    
+ 123   num = 12 , l = 3 , sum 3 
+        num = 1, l = 2 , sum 5 
+            
+      for each round chop off last digit from num
+      add it to sum  
+      
+      when num is single digit, add it to sum 
+      set num to sum, to keep the loop going   num = 11 
+      reset sum for next iterations 
  */
 var addDigits = function(num) {
-    if(num < 9 ) return num 
     
     let sum = 0 
     
