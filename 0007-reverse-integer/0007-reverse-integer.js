4e723ee848821443532bdf9427a6079bfd15987a
Bo Latt <83052118+bolattt@users.noreply.github.com>
2023-11-14 01:15:31 -0500

    Time: 74 ms (10.12%), Space: 44.1 MB (31.85%) - LeetHub

--- 0007-reverse-integer/0007-reverse-integer.js
+++ 0007-reverse-integer/0007-reverse-integer.js
@@ -4,21 +4,24 @@
  */
 var reverse = function(x) {
     let isNegative = x < 0
-
+    
     if(isNegative) x = x * - 1. // make it positive   
 
     let str = ''
     while(x > 0) { 
         let last = x % 10 
         x = Math.floor( x / 10) 
-
         str += last.toString()
-
     }
+    
     console.log(str)
     x = Math.abs(str)  // remove zeroes at the front 
-    if(x > Math.pow(2,31) - 1  ) return 0 
+    if(!isInRange(x)) return 0
     if(isNegative) x = x * - 1
-   
+
     return x 
 };
+
+function isInRange(x) { 
+    return x > Math.pow(-2,31) && x < Math.pow(2,31) - 1 
+}
