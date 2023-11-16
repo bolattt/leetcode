a238cb696ab2c26cfd6568359d305b95cdead016
Bo Latt <83052118+bolattt@users.noreply.github.com>
2023-11-15 20:18:47 -0500

    Time: 60 ms (83.15%), Space: 44.8 MB (32.16%) - LeetHub

--- 0905-sort-array-by-parity/0905-sort-array-by-parity.js
+++ 0905-sort-array-by-parity/0905-sort-array-by-parity.js
@@ -1,23 +1,7 @@
 /**
  * @param {number[]} nums
  * @return {number[]}
-    loop through the arry 
-        
-        do it in O(1) space 
-        [3,1,2,4,5,6,7]
-         l           r   
-        loop through the arr, while left is less than right 
-            if left is odd && right is even 
-                swap 
-                 l++, r-- 
-            if left is odd && right is odd 
-                move right -- 
-                when right is even, 
-                    swap 
-                l++, r-- 
-                
-    
-                
+   
     
  */
 var sortArrayByParity = function(nums) {
