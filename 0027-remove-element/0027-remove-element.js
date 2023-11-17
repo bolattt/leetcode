d7794b67133b17890e26ab99a93beb2ccb9d2c40
Bo Latt <83052118+bolattt@users.noreply.github.com>
2023-11-16 20:45:31 -0500

    Time: 58 ms (22.48%), Space: 42.3 MB (14.17%) - LeetHub

--- 0027-remove-element/0027-remove-element.js
+++ 0027-remove-element/0027-remove-element.js
@@ -9,7 +9,7 @@ var removeElement = function(nums, val) {
     let p = 0, i = 0; 
     while( i < nums.length) { 
         if(nums[i] != val) { 
-            [nums[i],nums[p]] = [ nums[p],nums[i] ]
+            nums[p] = nums[i]
             p++
         }
         i++ 
