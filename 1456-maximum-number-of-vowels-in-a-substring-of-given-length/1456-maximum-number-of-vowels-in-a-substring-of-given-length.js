9387638337512c785150f4282ed77ff92a1d1085
Bo Latt <83052118+bolattt@users.noreply.github.com>
2023-12-03 20:23:50 -0500

    Time: 69 ms (91.55%), Space: 44.8 MB (93.01%) - LeetHub

--- 1456-maximum-number-of-vowels-in-a-substring-of-given-length/1456-maximum-number-of-vowels-in-a-substring-of-given-length.js
+++ 1456-maximum-number-of-vowels-in-a-substring-of-given-length/1456-maximum-number-of-vowels-in-a-substring-of-given-length.js
@@ -5,17 +5,13 @@
  */
 var maxVowels = function(s, k) {
     let vowels = new Set('aeiou')
-    console.log(vowels)
-    // left = 0, sub = '' 
-    
-    // for loop 
-    let left = 0, subStr = '', maxVowels = 0, curVowels = 0 
+
+    let left = 0, maxVowels = 0, curVowels = 0 
     for(let right = 0; right < s.length; right++) { 
         let cur = s[right]
         if(vowels.has(cur)) { 
             curVowels++
         }
-        subStr += s[right] 
         
         if(right - left + 1 === k) { 
             if(curVowels > maxVowels) { 
