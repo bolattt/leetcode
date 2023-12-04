45ffc65395f4f176181f62df2eae132208846607
Bo Latt <83052118+bolattt@users.noreply.github.com>
2023-12-03 21:14:37 -0500

    Time: 52 ms (96.59%), Space: 44.1 MB (86.35%) - LeetHub

--- 1984-minimum-difference-between-highest-and-lowest-of-k-scores/1984-minimum-difference-between-highest-and-lowest-of-k-scores.js
+++ 1984-minimum-difference-between-highest-and-lowest-of-k-scores/1984-minimum-difference-between-highest-and-lowest-of-k-scores.js
@@ -2,19 +2,21 @@
  * @param {number[]} nums
  * @param {number} k
  * @return {number}
- [9,4,1,7,6], k = 3
+ [9,4,1,7,8,5,10] ,  k = 3 
+ [1,4,5,7,8,9,10] , k = 3 
+  sort the nums in ascending order 
+  In subArr of size k, left points at min val, right points at maxVal, elments in between don't matter
+  get the diff of max and min , update minDiff 
  */
 var minimumDifference = function(nums, k) {
     nums.sort((a,b) => a-b) 
     
     let left = 0, minDiff = Infinity
     for(let right = k - 1; right < nums.length; right++) { 
-           let diff = nums[right] - nums[left] 
-           minDiff = Math.min(minDiff, diff)
-            left++         
+       let diff = nums[right] - nums[left] 
+        minDiff = Math.min(minDiff, diff)
+        left++         
     }
     return minDiff
 };
 
-// [9,4,1,7,8,5,10] ,  k = 3 
-// [1,4,5,7,8,9,10] , k = 3 
