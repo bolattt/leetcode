3734f44d092bc73bff327d1fbb7c8891eac0a95a
Bo Latt <83052118+bolattt@users.noreply.github.com>
2023-12-03 20:44:16 -0500

    Time: 56 ms (27.49%), Space: 42 MB (75.70%) - LeetHub

--- 2269-find-the-k-beauty-of-a-number/2269-find-the-k-beauty-of-a-number.js
+++ 2269-find-the-k-beauty-of-a-number/2269-find-the-k-beauty-of-a-number.js
@@ -6,12 +6,11 @@
 var divisorSubstrings = function(num, k) {
     let str = num.toString(); 
     let left = 0, sub = '',count = 0; 
-     for(let right = 0; right < str.length; right++) { 
+    
+    for(let right = 0; right < str.length; right++) { 
         sub += str[right] 
-        console.log(sub)
         if( right - left  + 1 === k) { 
             let subNum = Number(sub)
-            console.log(subNum)
             let isDivisor = num % subNum == 0 
             if(isDivisor) { 
                 count++
