0aea9ac96deb605acdd7f3a7a2d1b94ae95c75d2
Bo Latt <83052118+bolattt@users.noreply.github.com>
2023-12-03 21:07:40 -0500

    Time: 52 ms (96.59%), Space: 44.8 MB (28.78%) - LeetHub

--- 1984-minimum-difference-between-highest-and-lowest-of-k-scores/1984-minimum-difference-between-highest-and-lowest-of-k-scores.js
+++ 1984-minimum-difference-between-highest-and-lowest-of-k-scores/1984-minimum-difference-between-highest-and-lowest-of-k-scores.js
@@ -8,16 +8,13 @@ var minimumDifference = function(nums, k) {
     nums.sort((a,b) => a-b) 
     
     let left = 0, minDiff = Infinity
-    for(let right = 0; right < nums.length; right++) { 
-        if(right - left + 1 === k) { 
+    for(let right = k - 1; right < nums.length; right++) { 
            let diff = nums[right] - nums[left] 
            minDiff = Math.min(minDiff, diff)
-            left++
-        }       
+            left++         
     }
     return minDiff
 };
 
 // [9,4,1,7,8,5,10] ,  k = 3 
 // [1,4,5,7,8,9,10] , k = 3 
-// 
