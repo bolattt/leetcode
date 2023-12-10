b25d0114a35603aac2e58cd9c033877bbfab510d
Bo Latt <83052118+bolattt@users.noreply.github.com>
2023-12-10 14:40:12 -0500

    Time: 63 ms (8.86%), Space: 43.6 MB (57.58%) - LeetHub

--- 1572-matrix-diagonal-sum/1572-matrix-diagonal-sum.js
+++ 1572-matrix-diagonal-sum/1572-matrix-diagonal-sum.js
@@ -8,10 +8,8 @@ var diagonalSum = function(mat) {
     for(let row = 0; row < mat.length; row++) { 
         let col = mat.length - row - 1 
         sum += mat[row][row]
-        sum += mat[row][col]
+        if(row != col) sum += mat[row][col]
      }
 
-    let center = Math.floor(mat.length / 2)
-    if(mat.length % 2 != 0) sum -= mat[center][center]
     return sum 
 };
