ab7ae252ef865b06aca6acb15173fe4fea0646b0
Bo Latt <83052118+bolattt@users.noreply.github.com>
2023-12-15 13:24:54 -0500

    Time: 71 ms (39.62%), Space: 45.5 MB (42.64%) - LeetHub

--- 0566-reshape-the-matrix/0566-reshape-the-matrix.js
+++ 0566-reshape-the-matrix/0566-reshape-the-matrix.js
@@ -6,20 +6,18 @@
  */
 var matrixReshape = function(mat, r, c) {
   
-    let isLegal = mat[0].length * mat.length == r *c 
+    const isLegal = mat[0].length * mat.length == r *c 
     if(!isLegal) return mat
     
-    let flat = mat.flat()
-    // let res = Array(r).fill(Array(c))
-    let res = []
+    const flat = mat.flat()
+    const res = []
     let i = 0 
     for(let row = 0; row < r; row++) { 
-        let temp = []
+        const temp = []
         for(let col = 0; col < c; col++) { 
             temp.push(flat[i++])
         }
         res.push(temp)
     } 
-    console.log(res)
     return res
 };
