bbd9080c6be7c92c7fe10fe55e2294c13b54e43e
Bo Latt <83052118+bolattt@users.noreply.github.com>
2023-12-17 11:07:48 -0500

    Time: 71 ms (5.60%), Space: 43.8 MB (73.77%) - LeetHub

--- 1436-destination-city/1436-destination-city.js
+++ 1436-destination-city/1436-destination-city.js
@@ -6,15 +6,15 @@
   after loop 
  */
 var destCity = function(paths) {
-    const citySet = new Set()
     const map = new Map()
+    
     for(let i = 0; i < paths.length; i++) { 
         map.set(paths[i][0],paths[i][1])
-        citySet.add(paths[i][1])
     }
-    for(let city of citySet) {
-        if(!map.has(city)){ 
-            return city
+    
+    for(let city of map) {
+        if(!map.has(city[1])){ 
+            return city[1]
         }
     }
 };
