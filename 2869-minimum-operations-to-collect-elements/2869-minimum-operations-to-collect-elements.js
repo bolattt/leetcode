575290e28f93c7abaec541d3a262fa6772cebe34
Bo Latt <83052118+bolattt@users.noreply.github.com>
2023-12-18 00:10:35 -0500

    Time: 57 ms (60.90%), Space: 43.7 MB (41.35%) - LeetHub

--- 2869-minimum-operations-to-collect-elements/2869-minimum-operations-to-collect-elements.js
+++ 2869-minimum-operations-to-collect-elements/2869-minimum-operations-to-collect-elements.js
@@ -6,27 +6,21 @@
         if it's less than k, 
  */
 var minOperations = function(nums, k) {
-    const res = []
-    const numSet = new Set(); 
+    const seen = new Set(); 
     let operations = 0
-    while(nums.length >= 1) { 
-        console.log('nums',nums)
-      numSet.add(nums.pop())
-        operations++
-     console.log(numSet)
-        
-      let missing = false
-      for(let i = 1; i <= k; i++) { 
-        if(!numSet.has(i)) { 
-            missing = true
-            break
-        }
-      }
-        
-       if(missing == false) { 
-        return operations
-       } 
-    }
     
+    while(nums.length >= 1) {
+        operations++
+        seen.add(nums.pop())
+       
+        let done = true
+        for(let i = 1; i <= k; i++) { 
+            if(!seen.has(i)) { 
+                done = false
+                break
+            }
+        }
+        if(done) return operations 
+    }   
 };
 
